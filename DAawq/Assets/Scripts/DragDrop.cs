0a7b8df09db06afef941aaf0cdcb6596c33bc448
ivanlazarovg <43958429+ivanlazarovg@users.noreply.github.com>
2021-11-13 19:53:56 +0200

    sq

--- DAawq/Assets/Scripts/DragDrop.cs
+++ DAawq/Assets/Scripts/DragDrop.cs
@@ -5,14 +5,40 @@ using UnityEngine;
 public class DragDrop : MonoBehaviour
 {
     public bool isDragging;
+    Upgrader[] upgradestuffs;
+    [SerializeField]
+    float radiusUpgrader;
+
+    private void Start()
+    {
+        upgradestuffs = FindObjectsOfType<Upgrader>();
+        
+    }
 
     private void OnMouseDown()
     {
+        
         isDragging = true;
     }
 
     private void OnMouseUp()
     {
+
+        float minDistance = 100;
+        GameObject pain = null;
+        foreach (var item in upgradestuffs)
+        {
+            float distance = Vector2.Distance(transform.position, item.transform.position);
+            if(distance < minDistance)
+            {
+                minDistance = distance;
+                pain = item.gameObject;
+            }
+        }
+        if(minDistance < radiusUpgrader)
+        {
+            transform.position = pain.transform.position;
+        }
         isDragging = false;
     }
     void Update()
